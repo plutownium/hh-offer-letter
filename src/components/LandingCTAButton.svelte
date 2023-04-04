08740f12f15817a6be34288dcf62dde92d800adc
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-04-04 14:39:13 -0700

    partial text copied

---
+++ src/components/LandingCTAButton.svelte
@@ -0,0 +1,8 @@
+<div class="w-72 h-16 bg-action">
+  <a
+    href="https://docs.google.com/document/d/1urfRfIJrueiEsHFxrBDROODdfbhS7fqeumFBzwqJdKE/edit?usp=sharing"
+    target="_blank"
+  >
+    <button class="h-full w-full text-xl text-off-white">Upgrade My Pay</button>
+  </a>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,9 +10,389 @@
 
   import '../app.css'
   import FloatyHeader from '../components/FloatyHeader.svelte'
+  import LandingCTAButton from '../components/LandingCTAButton.svelte'
 </script>
 
+<div>
+  <div>
+    <div>
+      <div>
+        <h1>
+          How to Add Hundreds of Thousands of Dollars to Your Lifetime Earnings With Only A Few
+          Hours of Work A Year
+        </h1>
+
+        <p>
+          The secret high earner playbook only a recruiter can tell you about… that you won’t get
+          from anyone else!
+        </p>
+
+        <p>This playbook guarantees you end up in the top 10% of earners for your role.</p>
+
+        <p>
+          Note: Regardless of recent tech layoffs, the method will net you a 20 to 40% increase in
+          salary.
+        </p>
+
+        <p>Are you ready for a pay increase?</p>
+
+        <LandingCTAButton />
+      </div>
+    </div>
+  </div>
+</div>
 <FloatyHeader />
+<div class="h-24" />
+<div class="w-full flex flex-col items-center bg-nigh-black">
+  <div class="w-11/12 sm:w-10/12 md:w-8/12 mt-8 h-full">
+    <div class="pb-12 h-3/5 flex items-center">
+      <h1 class="font-bold text-center text-off-white">
+        Uncover The Proven Method You Won’t Find On Google
+      </h1>
+
+      <h1>
+        Increase Your Pay As Much As Fifty Percent, Trigger Bidding Wars, and Join the Top Ten
+        Percent of Earners…
+      </h1>
+
+      <h1>Even If You Currently Struggle to Get Interviews!</h1>
+    </div>
+    <div class="h-2/5">
+      <div class="h-full px-4 sm:px-12 lg:px-24 xl:px-40 py-8 flex items-center bg-sand">
+        <h2 class="font-bold text-center text-nigh-black">
+          This is a 1718x ROI for anyone working in the following roles. (We did the math.)
+        </h2>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="pb-8 w-full flex flex-col items-center">
+  <div class="w-11/12 sm:w-10/12 md:w-8/12">
+    <!-- // placeholder -->
+    <h2>Are you a…</h2>
+    <ul>
+      <li>Software developer</li>
+      <li>Cybersecurity engineer</li>
+      <li>Systems engineer</li>
+      <li>Cloud engineer</li>
+      <li>Program manager</li>
+      <li>Project managers</li>
+      <li>Person in the above list who wants to see a $20,000 to $60,000 pay increase.</li>
+      <li>Person in the above list who wants to be in the top 5% of earners for their role.</li>
+    </ul>
+
+    <p>If you are, ask yourself:</p>
+
+    <p class="font-bold">
+      Have you found yourself thinking or saying something that would fit in with the quotes below?
+    </p>
+
+    <p>
+      “People say my role is high paying, but I don’t feel that way about it at all. I know I’m
+      underpaid.”
+    </p>
+
+    <p>“Even if I’m not exactly bottom 10% of earners, I still feel like I am.”</p>
+
+    <p>
+      “I send out hundreds of applications and receive a completely underwhelming response, even
+      though I supposedly have an ‘in demand’ skill.”
+    </p>
+
+    <p>“I’d rather be top 10% or even top 30% of income for my role.”</p>
+
+    <p>“I don’t care if the market is good or bad, I want to be in demand.”</p>
+  </div>
+</div>
+
+<div class="bg-nigh-black">
+  <StandardBox>
+    <PaddingBox
+      ><div class="py-12">
+        <h2 class="text-off-white">
+          <span class="font-bold">If this is you,</span> it means you’re about to find out how you could
+          increase your total compensation by 10 to 30% using a few hours of work.
+        </h2>
+      </div>
+      <div>
+        <p>
+          It means you get the opportunity to increase your compensation by tens of thousands of
+          dollars.
+        </p>
+
+        <p>You’ll be able to do this not just once, but over and over throughout your career.</p>
+
+        <p>You’ll leave your peers and colleagues in the industry in the dust monetarily.</p>
+
+        <p>How?</p>
+
+        <p>
+          By following a set of proven methods and techniques that hundreds of recruiters could tell
+          you about…
+        </p>
+
+        <p>But none will.</p>
+
+        <p>
+          Because improving your job hunting and negotiation abilities would be bad for their
+          clients, they don’t want you to improve.
+        </p>
+
+        <p class="underline">
+          In fact, recruiters and employers celebrate giving employees the short end of the stick in
+          salary negotiations.
+        </p>
+
+        <p>However, I’m here to help.</p>
+
+        <p>It doesn’t have to be this way.</p>
+
+        <p>
+          You don’t have to continue as an underpaid, undervalued member of your team for the rest
+          of your career.
+        </p>
+
+        <p>
+          You don’t have to disappoint yourself, year after year, by failing to secure a raise from
+          your employer.
+        </p>
+
+        <p>Or by avoiding the discussion entirely.</p>
+
+        <p>
+          The difficulty in securing a raise is not your doing. Society rigs the game against you,
+          and the only place you have to turn to for advice is the Internet.
+        </p>
+
+        <p>Yet the Internet isn’t always helpful!</p>
+      </div>
+      <div class="py-12">
+        <h2 class="text-off-white">
+          Look how many people on Reddit have the same problem getting paid what they’re worth:
+        </h2>
+      </div>
+      <div>
+        // todo: [ todo: insert gif]
+
+        <p>If advice on this topic is so common, why does the question come up over and over?</p>
+
+        <p>
+          Shouldn’t Redditors be able to solve this so well that the search function is all anyone
+          needs?
+        </p>
+
+        <p>Here’s typical Redditor advice you’ll receive about what to do when you’re underpaid:</p>
+
+        <!-- // todo: insert image -->
+        <p>The advice shows up again in this thread:</p>
+        <!-- // todo: insert image -->
+
+        <p>
+          The reason this advice shows up so often is that it works. But even when people apply,
+          they often come up short. Job hunting and interview skills often need work.
+        </p>
+
+        <p>
+          You can’t find better job offers if you can’t get to the interview stage… or make
+          interviewers love you as a candidate.
+        </p>
+
+        <p>Here’s another redditor echoing this same truth:</p>
+
+        <!-- // todo: insert image -->
+        <p>
+          And yet another who recommends interviewing with other companies to find out what you’re
+          worth:
+        </p>
+        <!-- // todo: insert image -->
+
+        <p>Yet none of these posts address the glaring, valley-wide room for improvement:</p>
+
+        <p>Your job hunting and interview skills.</p>
+
+        <p>If they’re weak, you’ll end up with the same low pay as you had before.</p>
+
+        <p>And nothing will change.</p>
+      </div>
+    </PaddingBox>
+  </StandardBox>
+</div>
+
+<!-- // todo: style bg color with gentle shade to alert reader -->
+<div class="bg-nigh-black">
+  <StandardBox>
+    <PaddingBox>
+      <div>
+        <h2>
+          “I have one of the highest demand skills in tech… yet I feel like I’m making minimum wage
+          all over again."
+        </h2>
+      </div>
+      <div>
+        <p>Ouch.</p>
+
+        <p>You thought your financial woes were over when you went into tech.</p>
+
+        <p>Society told you it’s an “in demand role,” yet you know you’re being underpaid.</p>
+
+        <p>Sites that report compensation say your compensation should be higher.</p>
+
+        <p>Friends from other companies say your compensation should be higher.</p>
+
+        <p>Your boss says your compensation… is right where it belongs!</p>
+
+        <p>
+          That it is “all they can afford at the moment” even though your coworker just got a raise.
+        </p>
+
+        <p>
+          You’d leave for another company if you could. Yet when you try to find a higher paying
+          role, it’s tough to find one!
+        </p>
+
+        <p>Aren’t your skills supposed to be in demand?</p>
+
+        <p>What’s going on here?</p>
+      </div>
+      <div>
+        <!-- // todo: color this entire box a color bg color so get attention -->
+        <h2>If you’d like to make the change described below, keep reading to find out how.</h2>
+      </div>
+    </PaddingBox>
+  </StandardBox>
+</div>
+
+<div>
+  <StandardBox>
+    <PaddingBox>
+      <div>
+        <h3>Before:</h3>
+        <p>
+          You feel undervalued and know it’s true. You find yourself pinching pennies when you never
+          thought you’d have to. Friends and people on forums say your pay should be higher. You
+          want to be the type of person who can brag and help others get ahead. But you don’t know
+          how to make a change.
+        </p>
+        <p>
+          Worse, you worry that the difficulties finding a job aren’t just about the resumes you’re
+          sending out, but about you personally. You worry that you’re going to be undervalued at
+          your role forever. Decades of it.
+        </p>
+      </div>
+
+      <div>
+        <h3>I’d like to show you how you can get from there, to here:</h3>
+      </div>
+
+      <div>
+        <h3>After:</h3>
+
+        <p>
+          You’re paid what you’re worth. Ahead of the pack. The companies you work for see you as
+          top talent, and pay accordingly.
+        </p>
+
+        <p>Your friends at other companies wish they knew your secret.</p>
+
+        <p>
+          You post a yearly “here’s my TC” and find yourself leading the pack on Team Blind. You
+          offer advice to people who are in your past position. (And it’s a referral to my program.)
+        </p>
+
+        <p>
+          You no longer worry; you know you’re a strong candidate who will have success after
+          success finding new roles with higher pay.
+        </p>
+      </div>
+      <div>
+        <h3>Would you rather have</h3>
+        <!-- // todo: "this' on " the left, "or this" on the right. -->
+        <!-- todo: style on mobile -->
+        <div class="flex">
+          <div>
+            <h4 class="underline">This...</h4>
+            <ul>
+              <li>
+                Turned down for a raise by your boss. He raises his hand to silence you mid pitch.
+              </li>
+              <li>See your biweekly payday hit your bank account and wish for more.</li>
+              <li>Know yourself as a financial underperformer</li>
+              <li>
+                Rejection letters pile up in your inbox every time you apply… but you get few
+                interviews.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h4 class="underline">Or this?</h4>
+
+            <ul>
+              <li>
+                Welcomed into day 1 of a new role with 37% higher TC; they say “glad we got you out
+                of there”
+              </li>
+              <li>
+                See your biweekly pay hit and feeling relieved, every time it happens, for weeks or
+                months
+              </li>
+              <li>Know your value and see it reflected in your bank statements.</li>
+              <li>A calendar full of job interviews for higher income roles.</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <p>If you find yourself living through the left column, you should know:</p>
+
+          <p>Your <span class="italic">job skills</span> don’t tell the whole story.</p>
+
+          <p class="underline">
+            Your <span class="font-bold">job hunting skills</span> contribute just as much if not more
+            to your compensation.
+          </p>
+        </div>
+      </div>
+    </PaddingBox>
+  </StandardBox>
+</div>
+
+<div>
+  <StandardBox>
+    <PaddingBox>
+      <div>
+        <h3>With the advice you receive today, you’ll have:</h3>
+        <ul>
+          <li>A calendar that has an interview on 9 of 10 work days.</li>
+          <li>Some days have more than one interview!</li>
+          <li>
+            Receive 4 interview requests in the same day… even though you only sent 100
+            applications.
+          </li>
+          <li>
+            House hunting trips into neighborhoods that were out of your price range a year ago.
+          </li>
+        </ul>
+
+        <h3>And you’ll avoid:</h3>
+
+        <ul>
+          <li>
+            Sending your resume out 2,000x only to get an offer that is “pretty much the same” as
+            what you have now.
+          </li>
+          <li>
+            Seeing people on /r/ITCareerQuestions talk about their raises… while your TC stays the
+            same.
+          </li>
+          <li>Being a productive member of the team year after year without recognition.</li>
+          <li>Working with the nagging feeling that you’re less than you’re worth.</li>
+          <li>Being “stuck” even though you got “one of the good jobs.”</li>
+          <li>Feeling disappointed in yourself.</li>
+          <li>Being politely declined a raise.</li>
+        </ul>
+      </div>
+    </PaddingBox>
+  </StandardBox>
+</div>
 
 <style>
   h1 {
