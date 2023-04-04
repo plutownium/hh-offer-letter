6256f3be52d08034d559af09539b072bdcdb5918
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-04-04 13:48:47 -0700

    add prettier for sveltekit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import BigBeautifulButton from '../components/BigBeautifulButton.svelte'
+  import ExtraPaddingLeft from '../components/ExtraPaddingLeft.svelte'
+  import GreenBox from '../components/GreenBox.svelte'
+  import PaddingBox from '../components/PaddingBox.svelte'
+  import RedBox from '../components/RedBox.svelte'
+  import StandardBox from '../components/StandardBox.svelte'
+  import TechniqueHeadline from '../components/TechniqueHeadline.svelte'
+  import WideBox from '../components/WideBox.svelte'
+
+  import '../app.css'
+  import FloatyHeader from '../components/FloatyHeader.svelte'
+</script>
+
+<FloatyHeader />
+
+<style>
+  h1 {
+    font-size: 2.4em;
+  }
+
+  h2 {
+    font-size: 1.8em;
+  }
+
+  .list-with-bullets > li {
+    font-size: 20px;
+  }
+
+  p,
+  li {
+    font-size: 20px;
+  }
+
+  .mixed-group > * {
+    margin-top: 18px;
+  }
+
+  .p-group > p {
+    margin-top: 18px;
+  }
+
+  .persuasion-tool p {
+    margin-top: 18px;
+  }
+
+  .text-off-white-container * {
+    color: #e5eef5;
+  }
+</style>
