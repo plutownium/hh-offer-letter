282855adf72e885d0e8001861f21292ceb28b9aa
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-04-04 15:12:58 -0700

    more text pasted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -262,137 +262,391 @@
   </StandardBox>
 </div>
 
-<div>
-  <StandardBox>
-    <PaddingBox>
-      <div>
-        <h3>Before:</h3>
-        <p>
-          You feel undervalued and know it’s true. You find yourself pinching pennies when you never
-          thought you’d have to. Friends and people on forums say your pay should be higher. You
-          want to be the type of person who can brag and help others get ahead. But you don’t know
-          how to make a change.
-        </p>
-        <p>
-          Worse, you worry that the difficulties finding a job aren’t just about the resumes you’re
-          sending out, but about you personally. You worry that you’re going to be undervalued at
-          your role forever. Decades of it.
-        </p>
-      </div>
+<StandardBox>
+  <PaddingBox>
+    <div>
+      <h3>Before:</h3>
+      <p>
+        You feel undervalued and know it’s true. You find yourself pinching pennies when you never
+        thought you’d have to. Friends and people on forums say your pay should be higher. You want
+        to be the type of person who can brag and help others get ahead. But you don’t know how to
+        make a change.
+      </p>
+      <p>
+        Worse, you worry that the difficulties finding a job aren’t just about the resumes you’re
+        sending out, but about you personally. You worry that you’re going to be undervalued at your
+        role forever. Decades of it.
+      </p>
+    </div>
 
-      <div>
-        <h3>I’d like to show you how you can get from there, to here:</h3>
-      </div>
+    <div>
+      <h3>I’d like to show you how you can get from there, to here:</h3>
+    </div>
 
-      <div>
-        <h3>After:</h3>
+    <div>
+      <h3>After:</h3>
 
-        <p>
-          You’re paid what you’re worth. Ahead of the pack. The companies you work for see you as
-          top talent, and pay accordingly.
-        </p>
+      <p>
+        You’re paid what you’re worth. Ahead of the pack. The companies you work for see you as top
+        talent, and pay accordingly.
+      </p>
 
-        <p>Your friends at other companies wish they knew your secret.</p>
+      <p>Your friends at other companies wish they knew your secret.</p>
 
-        <p>
-          You post a yearly “here’s my TC” and find yourself leading the pack on Team Blind. You
-          offer advice to people who are in your past position. (And it’s a referral to my program.)
-        </p>
+      <p>
+        You post a yearly “here’s my TC” and find yourself leading the pack on Team Blind. You offer
+        advice to people who are in your past position. (And it’s a referral to my program.)
+      </p>
 
-        <p>
-          You no longer worry; you know you’re a strong candidate who will have success after
-          success finding new roles with higher pay.
-        </p>
-      </div>
-      <div>
-        <h3>Would you rather have</h3>
-        <!-- // todo: "this' on " the left, "or this" on the right. -->
-        <!-- todo: style on mobile -->
-        <div class="flex">
-          <div>
-            <h4 class="underline">This...</h4>
-            <ul>
-              <li>
-                Turned down for a raise by your boss. He raises his hand to silence you mid pitch.
-              </li>
-              <li>See your biweekly payday hit your bank account and wish for more.</li>
-              <li>Know yourself as a financial underperformer</li>
-              <li>
-                Rejection letters pile up in your inbox every time you apply… but you get few
-                interviews.
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h4 class="underline">Or this?</h4>
-
-            <ul>
-              <li>
-                Welcomed into day 1 of a new role with 37% higher TC; they say “glad we got you out
-                of there”
-              </li>
-              <li>
-                See your biweekly pay hit and feeling relieved, every time it happens, for weeks or
-                months
-              </li>
-              <li>Know your value and see it reflected in your bank statements.</li>
-              <li>A calendar full of job interviews for higher income roles.</li>
-            </ul>
-          </div>
+      <p>
+        You no longer worry; you know you’re a strong candidate who will have success after success
+        finding new roles with higher pay.
+      </p>
+    </div>
+    <div>
+      <h3>Would you rather have</h3>
+      <!-- // todo: "this' on " the left, "or this" on the right. -->
+      <!-- todo: style on mobile -->
+      <div class="flex">
+        <div>
+          <h4 class="underline">This...</h4>
+          <ul>
+            <li>
+              Turned down for a raise by your boss. He raises his hand to silence you mid pitch.
+            </li>
+            <li>See your biweekly payday hit your bank account and wish for more.</li>
+            <li>Know yourself as a financial underperformer</li>
+            <li>
+              Rejection letters pile up in your inbox every time you apply… but you get few
+              interviews.
+            </li>
+          </ul>
         </div>
         <div>
-          <p>If you find yourself living through the left column, you should know:</p>
+          <h4 class="underline">Or this?</h4>
 
-          <p>Your <span class="italic">job skills</span> don’t tell the whole story.</p>
-
-          <p class="underline">
-            Your <span class="font-bold">job hunting skills</span> contribute just as much if not more
-            to your compensation.
-          </p>
+          <ul>
+            <li>
+              Welcomed into day 1 of a new role with 37% higher TC; they say “glad we got you out of
+              there”
+            </li>
+            <li>
+              See your biweekly pay hit and feeling relieved, every time it happens, for weeks or
+              months
+            </li>
+            <li>Know your value and see it reflected in your bank statements.</li>
+            <li>A calendar full of job interviews for higher income roles.</li>
+          </ul>
         </div>
       </div>
-    </PaddingBox>
-  </StandardBox>
-</div>
-
-<div>
-  <StandardBox>
-    <PaddingBox>
       <div>
-        <h3>With the advice you receive today, you’ll have:</h3>
-        <ul>
-          <li>A calendar that has an interview on 9 of 10 work days.</li>
-          <li>Some days have more than one interview!</li>
-          <li>
-            Receive 4 interview requests in the same day… even though you only sent 100
-            applications.
-          </li>
-          <li>
-            House hunting trips into neighborhoods that were out of your price range a year ago.
-          </li>
-        </ul>
+        <p>If you find yourself living through the left column, you should know:</p>
 
-        <h3>And you’ll avoid:</h3>
+        <p>Your <span class="italic">job skills</span> don’t tell the whole story.</p>
 
-        <ul>
-          <li>
-            Sending your resume out 2,000x only to get an offer that is “pretty much the same” as
-            what you have now.
-          </li>
-          <li>
-            Seeing people on /r/ITCareerQuestions talk about their raises… while your TC stays the
-            same.
-          </li>
-          <li>Being a productive member of the team year after year without recognition.</li>
-          <li>Working with the nagging feeling that you’re less than you’re worth.</li>
-          <li>Being “stuck” even though you got “one of the good jobs.”</li>
-          <li>Feeling disappointed in yourself.</li>
-          <li>Being politely declined a raise.</li>
-        </ul>
+        <p class="underline">
+          Your <span class="font-bold">job hunting skills</span> contribute just as much if not more
+          to your compensation.
+        </p>
       </div>
-    </PaddingBox>
-  </StandardBox>
-</div>
+    </div>
+  </PaddingBox>
+</StandardBox>
+
+<StandardBox>
+  <PaddingBox>
+    <div>
+      <h3>With the advice you receive today, you’ll have:</h3>
+      <ul>
+        <li>A calendar that has an interview on 9 of 10 work days.</li>
+        <li>Some days have more than one interview!</li>
+        <li>
+          Receive 4 interview requests in the same day… even though you only sent 100 applications.
+        </li>
+        <li>
+          House hunting trips into neighborhoods that were out of your price range a year ago.
+        </li>
+      </ul>
+
+      <h3>And you’ll avoid:</h3>
+
+      <ul>
+        <li>
+          Sending your resume out 2,000x only to get an offer that is “pretty much the same” as what
+          you have now.
+        </li>
+        <li>
+          Seeing people on /r/ITCareerQuestions talk about their raises… while your TC stays the
+          same.
+        </li>
+        <li>Being a productive member of the team year after year without recognition.</li>
+        <li>Working with the nagging feeling that you’re less than you’re worth.</li>
+        <li>Being “stuck” even though you got “one of the good jobs.”</li>
+        <li>Feeling disappointed in yourself.</li>
+        <li>Being politely declined a raise.</li>
+      </ul>
+    </div>
+    <div>
+      <p>
+        If you are a great software developer, but you aren’t adept at selling yourself and your
+        skills…
+      </p>
+
+      <p>You’ll be continually underpaid, no matter how good you are at your actual job.</p>
+
+      <p>
+        The facts are that <span class="underline">presentation and people skills matter</span>.
+      </p>
+
+      <p class="underline">Outreach and sales skills matter.</p>
+
+      <p>
+        Lots of software engineers, cybersecurity experts, etc. who are great at their technical
+        skills underperform in interviews.
+      </p>
+
+      <p>
+        And lots of managers who are great with people <span class="italic">still</span> underperform
+        in interviews!
+      </p>
+
+      <p>The good news is, this is mostly fixable with even a few weeks of work.</p>
+    </div>
+  </PaddingBox>
+</StandardBox>
+<StandardBox
+  ><PaddingBox>
+    <div>
+      <!-- // big white colored text against white bg  -->
+      <h2 class="text-green text-2xl">
+        To the degree that you’re willing to practice a handful of <span class="font-bold"
+          >well defined, doable exercises,</span
+        > you will be able to learn to interview like an expert negotiator.
+      </h2>
+    </div>
+    <div>
+      <p>Consider this image:</p>
+      <!-- // todo: r gos and s carrell img -->
+
+      <p>
+        Steve Carrell is dressed in a poorly fitted polo, baggy jeans and beat up running shoes.
+        Ryan Gosling’s character, in contrast, has a sharp fitted shirt, slacks and aesthetic
+        footwear. He also stands up straighter.
+      </p>
+
+      <p>We recognize that these are surface level traits, yet they are all we see.</p>
+
+      <p>Interviews are the same way.</p>
+
+      <p class="font-bold">
+        Luckily, we can quickly and easily improve our performance in the job search by changing how
+        we present ourselves.
+      </p>
+
+      <p>
+        Though our actual skills and productivity remain the same, by changing others’ perceptions
+        of them.
+      </p>
+
+      <p class="font-bold">
+        Our goal is for you to be the chad engineer, while the hiring manager is Steve Carrell’s
+        character.
+      </p>
+
+      <!-- todo: r gos meme with text -->
+
+      <p>You want the hiring manager to be wowed by your presentation.</p>
+
+      <p>That’s the money ticket.</p>
+
+      <p>Yet no one talks about the “how” of being a “wow” type of candidate.</p>
+
+      <p>Because they don’t know what to do, or how to define it.</p>
+
+      <p>Here’s more “advice” on the topic of being underpaid:</p>
+
+      <!-- todo: img 1, img 2 -->
+
+      <h3>
+        But we’re back to the same issue! If your skills are sharp, yet your offers don’t reflect
+        what you feel you’re worth…
+      </h3>
+
+      <p>What are you to do?</p>
+
+      <p>
+        The way it is now, <span class="underline"
+          >there’s no one on your team on this issue except you and me</span
+        >.
+      </p>
+
+      <h2>I’ll tell you how it really is:</h2>
+
+      <p>
+        You’re underpaid because the entire system is setup to hold you back from being well paid to
+        the best of its ability.
+      </p>
+
+      <p>Your boss wants your pay to be low.</p>
+
+      <p>Recruiters want your pay to be low.</p>
+
+      <p>Other companies want your pay to be low.</p>
+
+      <p>All of them have something in common: They’re out to help themselves, not you.</p>
+
+      <p>So how can you get ahead when everyone is trying to hold you back?</p>
+
+      <h2>You need to have an in-the-know IT recruiter who’s on your side.</h2>
+
+      <p>
+        You’ll learn a lot of hidden gems of wisdom that only experienced recruiters could tell you.
+      </p>
+
+      <p>Lots of them could tell you these secrets but won’t.</p>
+
+      <p>I’m the exception.</p>
+
+      <p>My 9-5 involves watching 30 companies make hiring decisions every single week.</p>
+
+      <p>I see who they hire… and who they don’t.</p>
+
+      <p>I see the patterns.</p>
+
+      <p>The stories.</p>
+
+      <p>The case by case example of what an in-demand candidate does and doesn’t do.</p>
+
+      <p>Yet I’m willing to help you.</p>
+
+      <p class="font-bold">
+        Because helping someone change careers to go from $40,000/year to $100,000/year is
+        satisfying.
+      </p>
+
+      <p class="font-bold">
+        Because helping someone go from $90,000/year to $130,000/year means I sleep great that
+        night.
+      </p>
+
+      <p>Last year I helped someone go from $20k to $90k. Felt great.</p>
+
+      <p>
+        In fact, I have so much to tell you that I can give away lots of advice <span
+          class="underline">for free</span
+        > and still have more to tell you after you invest.
+      </p>
+    </div>
+  </PaddingBox></StandardBox
+>
+
+<StandardBox
+  ><PaddingBox>
+    <div>
+      <h2>Four Free Ways to Improve Your Resume Today</h2>
+    </div>
+    <div>
+      <h3><span class="underline">Tip #1</span>: Use a two or three page resume.</h3>
+
+      <!-- todo: hh tweet img -->
+
+      <p>The “It has to fit on one page” advice is bullshit.</p>
+      <p class="font-bold">
+        Of the fifty candidates I helped get hired this year, 49 of them used a two or three page
+        resume.
+      </p>
+      <p>
+        The truth is that <span class="underline"
+          >if you use a one page resume, employers will presume that’s all you had to offer</span
+        >.
+      </p>
+      <p>Are you using a one page resume right now?</p>
+      <p>
+        So long as you follow the next tips, you’ll see an improvement by adding a second page.
+        (Supposing you don’t adhere to them already, anyway.)
+      </p>
+      <h3>Tip #2: Use a simple low-design approach.</h3>
+
+      <p>
+        While you don’t want to present a document with zero formatting, making the resume flashy is
+        also the wrong approach.
+      </p>
+      <p>Recruiters will throw out a resume that looks flashy before they even read it.</p>
+      <p>You want your features and benefits to stand on their own.</p>
+      <p>The design takes attention away from the bullet points.</p>
+      <p>So by using low-design, you allow the bullet points to “speak for themselves.”</p>
+      <h3>Tip #3: Use language from the job post you are applying to.</h3>
+
+      <p>It takes far more time, but the upside is huge: Recruiters will recognize</p>
+      <p>Your application really was crafted specifically for that posting.</p>
+      <p>Your skills really match exactly what they asked for.</p>
+      <p>Without your application matching the job post’s language…</p>
+      <p>The recruitment team has to do a lot of thinking to understand how you’d fill the role.</p>
+      <p>When you use language that matches the job post, they understand exactly:</p>
+      <p>“Oh yeah, s/he would be able to fulfill requirements X, Y and Z.”</p>
+      <p>That means you’ll get more interviews.</p>
+      <p>That means you’ll be in demand.</p>
+      <p>That means you’ll be offered more money.</p>
+      <h3>Tip #4: Using specific detailed bullet points that are quantified.</h3>
+
+      <p>If you aren’t in the habit of doing this, it will take work to start doing it.</p>
+      <p>But…</p>
+      <p>It will be well worth the effort.</p>
+      <p>Don’t say: “Reduced call time from the database.”</p>
+      <p>
+        Do say: “Reduced call time from the database by 37% by decreasing read algorithms from
+        O(nlogn) to O(n).”
+      </p>
+      <p>
+        Don’t say: “Decreased page load speed by converting to Tailwind, yielding a decrease in the
+        bounce rate.”
+      </p>
+      <p>
+        Do say: “Decreased page load speed by 48% by converting from CSS to Tailwind, yielding a 26%
+        decrease in the bounce rate.”
+      </p>
+
+      <p>
+        Similar to Tip #2, the issue is that the vague option leaves recruiters to fill in the
+        blanks themselves.
+      </p>
+      <p>(And they don’t fill them in. Because it would take effort and require presumptions.)</p>
+      <p>
+        By telling them specifically, quantifiably, what you changed during your previous roles, you
+        go from…
+      </p>
+      <p>“Meh, not sure what this means.”</p>
+      <p>To…</p>
+      <p>“Wow, that would be such an improvement here.”</p>
+      <p class="font-bold">These tips work and work well.</p>
+      <p class="font-bold">Applying them will get you a better response rate immediately.</p>
+      <p class="font-bold">
+        But you’ll get far further by investing in getting the full Headhunter treatment.
+      </p>
+
+      <p>If these tips are “all you need,” I wish you the best…</p>
+    </div>
+  </PaddingBox></StandardBox
+>
+
+<StandardBox
+  ><PaddingBox>
+    <div>
+      <h2>
+        If you’re not the type of person to ask for help from an expert, the above tips will be all
+        you need to make sure you’re “dialing in the basics.”
+      </h2>
+
+      <h2>
+        But if you sense that <span class="underline"
+          >investing in your career’s success is the right move</span
+        >, and you’d like to receive <span class="underline">recruiter secrets</span>…
+      </h2>
+
+      <h2>Read on to find out everything you’d need to know to make an informed decision.</h2>
+    </div>
+  </PaddingBox></StandardBox
+>
 
 <style>
   h1 {
