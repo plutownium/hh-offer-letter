a64866866850f14700c6f92992f646e6d412eca5
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-04-04 20:21:15 -0700

    landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,33 +11,51 @@
   import '../app.css'
   import FloatyHeader from '../components/FloatyHeader.svelte'
   import LandingCTAButton from '../components/LandingCTAButton.svelte'
+  import Logo from '../components/Logo.svelte'
 </script>
 
-<div class="h-screen bg-">
-  <div>
-    <div>
-      <div class="">
-        <h1>
-          How to Add Hundreds of Thousands of Dollars to Your Lifetime Earnings With Only A Few
-          Hours of Work A Year
-        </h1>
+<!-- todo: verify looks good on mobile & tablet -->
+<div class="h-full bg-celeste">
+  <div class="w-full h-full flex justify-center items-center">
+    <div class="w-3/5 h-full">
+      <div class="mt-24 mb-12">
+        <Logo />
+      </div>
 
-        <p>
-          The secret high earner playbook only a recruiter can tell you about… that you won’t get
-          from anyone else!
-        </p>
+      <h1 class="mt-8 mb-4 text-3xl font-bold text-blue">
+        The secret high earner playbook only a recruiter can tell you about.
+      </h1>
 
-        <p>This playbook guarantees you end up in the top 10% of earners for your role.</p>
+      <h1 class="text-jet text-5xl font-bold">
+        How to Add Hundreds of Thousands of Dollars to Your Lifetime Earnings With Only A Few Hours
+        of Work A Year
+      </h1>
 
-        <p>
-          Note: Regardless of recent tech layoffs, the method will net you a 20 to 40% increase in
-          salary.
-        </p>
+      <!-- <h1 class="mt-8 text-5xl">That you won’t get from anyone else!</h1> -->
 
-        <p>Are you ready for a pay increase?</p>
+      <p class="mt-8 text-2xl">
+        This playbook <span class="underline">guarantees</span> you end up in the
+        <span class="text-blue font-bold">top 10% of earners</span>
+        for your role.
+      </p>
 
+      <p class="mt-8 text-2xl">
+        Note: <span class="underline text-orange font-bold">Regardless of recent tech layoffs</span
+        >, this method will net you a 20 to 40% increase in salary.
+      </p>
+
+      <p class="mt-8 text-2xl">Are you ready for a pay increase?</p>
+
+      <div class="mt-8 mb-16 w-full flex justify-center">
         <LandingCTAButton />
       </div>
     </div>
   </div>
 </div>
+
+<style>
+  /* todo: make font work */
+  * {
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+</style>
